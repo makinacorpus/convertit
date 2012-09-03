9aa7bff909e8d84555545708cf6a2c387a5d07f5
Alex Marandon <al@formation1.(none)>
2012-09-03 15:51:51 +0200

    Implement basic ODT -> PDF from URL

--- topdfserver/views.py
+++ topdfserver/views.py
@@ -1,10 +1,20 @@
+import os
+import subprocess
+import urllib2
 from mimetypes import guess_type
+from uuid import uuid4
+
 from pyramid.view import view_config
+from pyramid.url import static_url
 from pyramid.httpexceptions import HTTPBadRequest, HTTPFound
 
 
-def odt_to_pdf(filepath):
-    pass
+def odt_to_pdf(filepath, target_dir):
+    command = ['unoconv', '--outputpath', target_dir, '--format', 'pdf', filepath]
+    subprocess.call(command)
+    basename = os.path.basename(filepath)
+    filename, ext = os.path.splitext(basename)
+    return os.path.join(target_dir, filename + '.pdf')
 
 
 converters = {
@@ -24,16 +34,20 @@ def home_view(request):
     if mimetype not in converters:
         return HTTPBadRequest('Unsupported mimetype %s' % mimetype)
 
-    filepath = download_file(url)
-    converted_filepath = converters[mimetype](filepath)
-    converted_file_url = path_to_url(converted_filepath)
+    target_dir = request.registry.settings['download_dir']
+    filepath = download_file(url, target_dir)
+    static_dir = request.registry.settings['static_dir']
+    target_dir = os.path.join(static_dir, 'converted')
+    converted_filepath = converters[mimetype](filepath, target_dir)
 
-    return HTTPFound(converted_file_url)
-
-
-def download_file(url):
-    pass
+    return HTTPFound(static_url(converted_filepath, request))
 
 
-def path_to_url(filepath):
-    pass
+def download_file(url, target_dir):
+    _, ext = os.path.splitext(url)
+    data = urllib2.urlopen(url).read()
+    filename = "%s%s" % (uuid4(), ext)
+    target_file = os.path.join(target_dir, filename) 
+    with open(target_file, 'w') as f:
+        f.write(data)
+    return target_file
