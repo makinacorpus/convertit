fd59cce9839969e3f08ef9cf87bbe1cfd1df6040
Antoine Cezar <antoine.cezar@gmail.com>
2012-09-03 17:02:02 +0200

    Modified converted files serving

--- topdfserver/__init__.py
+++ topdfserver/__init__.py
@@ -11,10 +11,12 @@ def create_dir(dirname):
 def main(global_config, **settings):
     """ This function returns a Pyramid WSGI application.
     """
-    create_dir(settings['static_dir'])
+    create_dir(settings['converted_dir'])
     create_dir(settings['download_dir'])
     config = Configurator(settings=settings)
-    config.add_static_view('static', settings['static_dir'], cache_max_age=3600)
+    config.add_static_view('static', 'static', cache_max_age=3600)
+    config.add_static_view('converted', settings['converted_dir'],
+        cache_max_age=3600)
     config.add_route('home', '/')
     config.scan()
     return config.make_wsgi_app()
