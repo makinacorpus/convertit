227a71597e669446069f30f38489be8dcac43380
Antoine Cezar <antoine.cezar@gmail.com>
2012-09-03 14:10:13 +0200

    Created odt to pdf view structure.

--- topdfserver/views.py
+++ topdfserver/views.py
@@ -1,5 +1,39 @@
+from mimetypes import guess_type
 from pyramid.view import view_config
+from pyramid.httpexceptions import HTTPBadRequest, HTTPFound
 
-@view_config(route_name='home', renderer='templates/mytemplate.pt')
-def my_view(request):
-    return {'project':'topdfserver'}
+
+def odt_to_pdf(filepath):
+    pass
+
+
+converters = {
+    'application/vnd.oasis.opendocument.text': odt_to_pdf
+}
+
+
+@view_config(route_name='home')
+def home_view(request):
+    url = request.GET.get('url')
+
+    if url is None:
+        return HTTPBadRequest('Missing parameter: url')
+
+    mimetype = guess_type(url)[0]
+
+    if mimetype not in converters:
+        return HTTPBadRequest('Unsupported mimetype %s' % mimetype)
+
+    filepath = download_file(url)
+    converted_filepath = converters[mimetype](filepath)
+    converted_file_url = path_to_url(converted_filepath)
+
+    return HTTPFound(converted_file_url)
+
+
+def download_file(url):
+    pass
+
+
+def path_to_url(filepath):
+    pass
