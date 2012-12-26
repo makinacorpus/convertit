583e467a7b63320722905b74cc193e7068862538
Antoine Cezar <antoine.cezar@makina-corpus.com>
2012-12-26 15:22:23 +0100

    Fix forgotten deprecated test

--- convertit/tests/test_helpers.py
+++ convertit/tests/test_helpers.py
@@ -2,28 +2,7 @@ import unittest
 from mock import patch
 from datetime import datetime
 
-from ..helpers import url_to_filename
 from ..helpers import remove_files_older_than
-
-
-class UrlToFilenameTests(unittest.TestCase):
-    url_with_basename = {
-        u'http://user:pass@host.tld/path/to/file': 'host-tld-path-to-file',
-        u'http://user:pass@host.tld/path/to/file.ext': 'host-tld-path-to-file'
-    }
-    url_without_basename = {
-        u'http://user:pass@host.tld/path/to/': 'host-tld-path-to',
-    }
-
-    def test_url_with_basename(self):
-        for url, expected_filename in self.url_with_basename.items():
-            filename = url_to_filename(url)
-            self.assertEqual(filename, expected_filename)
-
-    def test_url_without_basename(self):
-        for url, expected_filename in self.url_without_basename.items():
-            filename = url_to_filename(url)
-            self.assertEqual(filename, expected_filename)
 
 
 class RemoveFilesOlderThanTests(unittest.TestCase):
