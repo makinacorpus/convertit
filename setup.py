e1140e9cf284754417764f1bd7484b0d8c3101a1
Gael UTARD <gael.utard@kisis.fr>
2014-12-18 11:27:04 +0100

    Back to development: 1.1.2

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ test_requires = requires + [
 
 
 setup(name='convertit',
-      version='1.1.1',
+      version='1.1.2dev',
       description='A file conversion Web API in Pyramid',
       long_description=README + '\n\n' + CHANGES,
       license='AGPLV3',
